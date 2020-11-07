cbc4e108fa29c4815546cc92a0359ca324e1abee
Mustafa Agamey <eng.mustafaelagamey@gmail.com>
2020-11-07 12:23:20 +0200

    Create First template

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Hello World</h1>')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
