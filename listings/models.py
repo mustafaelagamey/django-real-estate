aaba792614b3423fdcd1ee314cc5dba156402f38
Mustafa Agamey <eng.mustafaelagamey@gmail.com>
2020-11-14 23:09:51 +0200

    Added - Add model listing with its migration

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,15 @@
+from datetime import datetime
+
 from django.db import models
 
+
 # Create your models here.
+class Listing(models.Model):
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    garage = models.IntegerField()
+    sqft = models.IntegerField()
+    lot_size = models.DecimalField(max_digits=3, decimal_places=1)
+    listing_date = models.DateTimeField(default=datetime.now)
+    description = models.TextField()
