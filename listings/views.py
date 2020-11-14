9322cb9ff673415feebbbc38757f4ba80ef4cc32
Mustafa Agamey <eng.mustafaelagamey@gmail.com>
2020-11-15 00:42:44 +0200

    Added - Fetching listings from DB and display them

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import Listing
 
 
 # Create your views here.
 def listings_index(request):
-    return render(request, 'listings/listings.html')
+    context = {
+        'listings': Listing.objects.all()
+    }
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request,listing_id):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
